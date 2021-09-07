ad91cb039e511abd91b2a0ad68f59cf8bb3076f7
shearpaladin <sandy6@ualberta.ca>
2021-09-06 18:42:25 -0600

    did not realize there was a raw button on github..

--- version.py
+++ version.py
@@ -6,6 +6,6 @@ get_request = requests.get("https://www.google.com/")
 print("Retrieving Google Home Page Contents\n", get_request.content)
 
 
-get_script = requests.get("https://raw.githubusercontent.com/shearpaladin/CMPUT404-LAB1/blob/main/version.py")
+get_script = requests.get("https://raw.githubusercontent.com/shearpaladin/CMPUT404-LAB1/main/version.py")
 
 print(get_script.text)
