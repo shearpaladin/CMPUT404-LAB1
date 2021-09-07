e9676a062bf4e2626392bc0e88f37a73bfd94ea9
shearpaladin <sandy6@ualberta.ca>
2021-09-06 18:46:01 -0600

    Complete script retrieval

--- version.py
+++ version.py
@@ -1,11 +1,11 @@
 import requests
 
-print("Requests version: " + requests.__version__)
+print("\nRequests version: " + requests.__version__)
 
 get_request = requests.get("https://www.google.com/")
-print("Retrieving Google Home Page Contents\n", get_request.content)
+print("\nRetrieving Google Home Page Contents\n", get_request.content)
 
-
+print("\nRetrieving Python Script...\n")
 get_script = requests.get("https://raw.githubusercontent.com/shearpaladin/CMPUT404-LAB1/main/version.py")
 
 print(get_script.text)
