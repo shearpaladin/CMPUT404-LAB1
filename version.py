37af93462b7ad50d2745696873b0253af531a09c
shearpaladin <sandy6@ualberta.ca>
2021-09-06 18:41:09 -0600

    uploaded raw content url

--- version.py
+++ version.py
@@ -4,3 +4,8 @@ print("Requests version: " + requests.__version__)
 
 get_request = requests.get("https://www.google.com/")
 print("Retrieving Google Home Page Contents\n", get_request.content)
+
+
+get_script = requests.get("https://raw.githubusercontent.com/shearpaladin/CMPUT404-LAB1/blob/main/version.py")
+
+print(get_script.text)
